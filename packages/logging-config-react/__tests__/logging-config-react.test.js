38887dc0c0c2b9b82e3dec9b3e6d020ba6f382d8
Stuart Thomson <me@sthom.kiwi>
2020-02-16 19:17:05 +1300

    Add context to get configuration passed down the tree

--- packages/logging-config-react/__tests__/logging-config-react.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const loggingConfigReact = require('..');
-
-describe('@sthom/logging-config-react', () => {
-    it('needs tests');
-});
--- packages/logging-config-react/src/logging-config-react.ts
+++ packages/logging-config-react/src/logging-config-react.ts
@@ -1,6 +1,27 @@
-import Logger, { DEFAULT_CONFIGURATION } from "@sthom/logging-config";
-import { useMemo } from "react";
+import Logger, {
+  DEFAULT_CONFIGURATION,
+  LoggingConfiguration
+} from "@sthom/logging-config";
+import React, { useMemo, useContext } from "react";
 
-export default function useLogger(config = DEFAULT_CONFIGURATION) {
-  return useMemo(() => new Logger(config), [config]);
+const loggerContext = React.createContext(DEFAULT_CONFIGURATION);
+
+/**
+ * Context for LoggingConfiguration
+ */
+export const LoggingConfigurationContext = loggerContext.Provider;
+
+/**
+ * Hook to get a Logger instance.
+ *
+ * Use in conjunction with LoggingConfigurationContext to simplify logging
+ * @param config Configuration object for this logger
+ */
+export default function useLogger(config?: LoggingConfiguration) {
+  const contextConfig = useContext(loggerContext);
+
+  // Use the configuration passed in first, otherwise use the context provided one
+  const configToUse = config ?? contextConfig;
+
+  return useMemo(() => new Logger(configToUse), [configToUse]);
 }
