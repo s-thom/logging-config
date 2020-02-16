2145ee2c96a5a37bfe968c57717a8a6840635774
Stuart Thomson <me@sthom.kiwi>
2020-02-16 21:00:30 +1300

    Add tag parameter to useLogger hook

--- packages/logging-config-react/src/logging-config-react.ts
+++ packages/logging-config-react/src/logging-config-react.ts
@@ -2,7 +2,7 @@ import Logger, {
   DEFAULT_CONFIGURATION,
   LoggingConfiguration
 } from "@sthom/logging-config";
-import React, { useMemo, useContext } from "react";
+import React, { useContext, useMemo } from "react";
 
 const loggerContext = React.createContext(DEFAULT_CONFIGURATION);
 
@@ -14,14 +14,15 @@ export const LoggingConfigurationContext = loggerContext.Provider;
 /**
  * Hook to get a Logger instance.
  *
- * Use in conjunction with LoggingConfigurationContext to simplify logging
+ * In order to change configuration of the logger, it's recommended to use LoggingConfigurationContext.
+ * @param tag Tag for this logger
  * @param config Configuration object for this logger
  */
-export default function useLogger(config?: LoggingConfiguration) {
+export default function useLogger(tag?: string);
+export default function useLogger(tag?: string, config?: LoggingConfiguration);
+export default function useLogger(tag?: string, config?: LoggingConfiguration) {
   const contextConfig = useContext(loggerContext);
-
-  // Use the configuration passed in first, otherwise use the context provided one
   const configToUse = config ?? contextConfig;
 
-  return useMemo(() => new Logger(configToUse), [configToUse]);
+  return useMemo(() => new Logger(tag, configToUse), [tag, configToUse]);
 }
